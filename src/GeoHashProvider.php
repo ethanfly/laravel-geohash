b6232ab7f3df7ab934c5a5f1019f40e924966702
Ethanfly <ethanfly@qq.com>
2019-08-22 15:19:12 +0800

    first

---
+++ src/GeoHashProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Geohash;
+
+use Illuminate\Support\ServiceProvider;
+
+class GeoHashProvider extends ServiceProvider
+{
+    /**
+     * 服务提供者加是否延迟加载
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        $this->app->singleton(Geohash::class, function (){
+            return new GeoHash();
+        });
+    }
+
+    /**
+     * 获取由提供者提供的服务
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [Geohash::class];
+    }
+
+
+}
