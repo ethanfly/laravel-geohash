6eb681869dfa7bd07ed8a2960d3481540cd65baa
Ethanfly <ethanfly@qq.com>
2019-08-22 15:44:28 +0800

    add get distance

--- src/GeoHash.php
+++ src/GeoHash.php
@@ -256,4 +256,38 @@ class Geohash
             return $this->binDecode($binary, $min, $mid);
         }
     }
+
+    /***
+     * 获取两点距离(经纬度)
+     * @param $lat1
+     * @param $lng1
+     * @param $lat2
+     * @param $lng2
+     * @return float
+     */
+    function GetDistanceByPoint($lat1, $lng1, $lat2, $lng2)
+    {
+        //将角度转为狐度
+        $radLat1 = deg2rad($lat1);//deg2rad()函数将角度转换为弧度
+        $radLat2 = deg2rad($lat2);
+        $radLng1 = deg2rad($lng1);
+        $radLng2 = deg2rad($lng2);
+        $a = $radLat1 - $radLat2;
+        $b = $radLng1 - $radLng2;
+        $s = 2 * asin(sqrt(pow(sin($a / 2), 2) + cos($radLat1) * cos($radLat2) * pow(sin($b / 2), 2))) * 6371.393;
+        return round($s, 1);
+    }
+
+    /***
+     * 获取距离(hash值)
+     * @param $hash1
+     * @param $hash2
+     * @return float
+     */
+    function GetDistanceByHash($hash1, $hash2)
+    {
+        list($lat1, $lng1) = $this->decode($hash1);
+        list($lat2, $lng2) = $this->decode($hash2);
+        return $this->GetDistanceByPoint($lat1, $lng1, $lat2, $lng2);
+    }
 }
